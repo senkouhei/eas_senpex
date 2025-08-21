9e57b0891e1b2a56e22511dfdc92fd95ff36797d
endless-runner-007 <cleancorder8k@hotmail.com>
2025-08-21 15:04:46 +0900

    add body-parser for sms

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import http from 'http';
@@ -24,7 +25,7 @@ import analyticsRoutes from './routes/analytics.js';
   // Middleware
   app.use(cors());
   app.use(express.json());
-
+  app.use(bodyParser.urlencoded({ extended: false }));
   // Google Service
   // getGoogleServiceInstance();
 
